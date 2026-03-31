e0f2e6fe19db21d9eeec20683bdb6c58c135c083
João Fassina <jf.fassina@gmail.com>
2026-03-31 00:00:31 -0300

    feat: OpenGL DrawSquare Implementation

--- src/main/java/engine/scenes/Scene.java
+++ src/main/java/engine/scenes/Scene.java
@@ -2,14 +2,16 @@ package engine.scenes;
 
 public abstract class Scene {
 
-
     public Scene(){
-
 
     }
 
-    //TODO: Game Obj, Render, PhysicsHandle
+    public void init(){
 
+    }
+
+
+    //TODO: Game Obj, Render, PhysicsHandle
     public abstract void update(float deltaTime);
 
 }
